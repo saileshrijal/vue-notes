d89ec0f958697b8392ec50ca65d4bbb416fd1739
Abhishek Bohora <abhishek.bohora16@gmail.com>
2023-01-27 15:43:13 +0545

    updated pagination

---
+++ src/views/category/composables/useFetch.js
@@ -0,0 +1,20 @@
+import { ref } from "vue";
+import axios from "axios";
+
+export default function useFetch(endpoint) {
+  const data = ref([]);
+  const error = ref(null);
+  const loading = ref(true);
+  async function fetchData() {
+    try {
+      const response = await axios.get(endpoint);
+      data.value = response.data;
+    } catch (err) {
+      error.value = err;
+    } finally {
+      loading.value = false;
+    }
+  }
+  fetchData();
+  return { data, error, loading };
+}
