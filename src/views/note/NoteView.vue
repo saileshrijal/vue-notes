5941e9a1bf83d8f8bb8223047dcec162ab04a383
Abhishek Bohora <abhishek.bohora16@gmail.com>
2023-01-28 15:05:33 +0545

    notes-pagination-added

--- src/views/note/NoteView.vue
+++ src/views/note/NoteView.vue
@@ -5,36 +5,51 @@
         <h2 class="text-uppercase">List Of Notes</h2>
       </div>
       <div class="col-6 text-end">
-        <button class="btn btn-dark" @click="addClicked">Add Notes</button>
+        <button class="btn btn-dark" @click="addClicked">Add Note</button>
       </div>
       <hr />
       <div class="card">
         <div class="card-body">
-          <div class="container-fluid my-1 bg-dark p-2 rounded text-white" v-for="(note) in notes" :key="note.id">
-            <div class="row">
-              <div class="col-8 d-flex flex-column align-items-start justify-content-center">
-                <h4>{{ note.title }}</h4>
-                <p>{{ note.description }}</p>
-              </div>
-              <div class="col-4 d-flex flex-column align-items-end justify-content-center gap-2">
-                <div>
-                  <li style="font-size: 10px;"
-                    class="text-uppercase fw-bold bg-white d-inline-block rounded text-black px-2 py-1">{{
-                      note.category.title
-                    }}</li>
-                </div>
-                <div class="">
-                  <button class="btn btn-sm btn-primary" @click="onEdit(note.id)">
-                    Edit
-                  </button>
-                  |
-                  <button class="btn btn-sm btn-danger" @click="onDelete(note.id)">
-                    Delete
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Note Title</th>
+                <th scope="col">Description</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <transition-group name="list">
+                <tr v-for="(note, index) in notes.data" :key="note.id">
+                  <th scope="row">{{ index + 1 }}</th>
+                  <td>{{ note.title }}</td>
+                  <td>{{ note.description }}</td>
+                  <td>
+                    <button
+                      class="btn btn-sm btn-primary"
+                      @click="onEdit(note.id)"
+                    >
+                      Edit
+                    </button>
+                    |
+                    <button
+                      class="btn btn-sm btn-danger"
+                      @click="onDelete(note.id)"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              </transition-group>
+            </tbody>
+          </table>
+          <Pagination
+            v-model="notes.page"
+            :records="notes.totalCount"
+            :per-page="notes.pageSize"
+            @paginate="myCallback"
+          />
         </div>
       </div>
     </div>
@@ -46,11 +61,25 @@ import { ref, onMounted } from "vue";
 import axios from "axios";
 import router from "@/router";
 import swal from "sweetalert2";
-const notes = ref([]);
+import Pagination from "v-pagination-3";
+const notes = ref({
+  data: [],
+  page: 1,
+  pageSize: 10,
+  totalCount: 0,
+  totalPages: 0,
+});
+
+const myCallback = async (page) => {
+  notes.value.page = page;
+  await getNotes();
+};
 
 const getNotes = async () => {
   try {
-    const response = await axios.get("https://localhost:44385/api/Note/GetAll");
+    const response = await axios.get(
+      `https://localhost:44385/api/Note/GetAll?page=${notes.value.page}&pageSize=${notes.value.pageSize}`
+    );
     if (response.status === 200) {
       notes.value = response.data;
       // console.log(response.data);
@@ -112,3 +141,21 @@ onMounted(async () => {
   await getNotes();
 });
 </script>
+
+<style scoped>
+.list-move,
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(-50px);
+}
+
+.list-leave-active {
+  position: absolute;
+}
+</style>
